8e09efb6d24b4b36b7175e2e3fe0dc710c6760be
wanghanyu8 <wanghanyu8@jd.com>
2017-11-27 16:07:21 +0800

    io, memory top

---
+++ src/main/java/system/IO_TOP.java
@@ -0,0 +1,10 @@
+package system;
+
+/**
+ * pidstat -d -t -p pid
+ *
+ * @author wanghanyu
+ * @create 2017-11-27 16:04
+ */
+public class IO_TOP {
+}
---
+++ src/main/java/system/MEM_TOP.java
@@ -0,0 +1,10 @@
+package system;
+
+/**
+ * pidstat -r -t -p pid
+ *
+ * @author wanghanyu
+ * @create 2017-11-27 16:05
+ */
+public class MEM_TOP {
+}
