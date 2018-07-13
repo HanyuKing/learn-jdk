7ec6e103c639c55ab6295918e729fcd8a4c6d511
wanghanyu8 <wanghanyu8@jd.com>
2018-07-13 12:03:19 +0800

    String Intern

--- src/main/java/other/StringInternTest.java
+++ src/main/java/other/StringInternTest.java
@@ -1,6 +1,12 @@
 package other;
 
+import org.junit.Test;
+
 /**
+ * refer  https://tech.meituan.com/in_depth_understanding_string_intern.html?utm_source=tool.lu
+ *
+ * String.intern method test, different with jdk6 and jdk7
+ *
  * @author wanghanyu
  * @create 2017-10-28 16:33
  */
@@ -11,5 +17,31 @@ public class StringInternTest {
 
         String a = new String("ja") + "va";
         System.out.println(a.intern() == a);
+    }
+
+    @Test
+    public void testStringIntern2() {
+        String s = new String("1");
+        s.intern();
+        String s2 = "1";
+        System.out.println(s == s2);
+
+        String s3 = new String("1") + new String("1");
+        s3.intern();
+        String s4 = "11";
+        System.out.println(s3 == s4);
+    }
+
+    @Test
+    public void testStringIntern() {
+        String s = new String("1");
+        String s2 = "1";
+        s.intern();
+        System.out.println(s == s2);
+
+        String s3 = new String("1") + new String("1");
+        String s4 = "11";
+        s3.intern();
+        System.out.println(s3 == s4);
     }
 }
