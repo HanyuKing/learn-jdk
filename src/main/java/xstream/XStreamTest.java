4bd190849d4df8b95cc4f79e4b1418eba98c3dbb
wanghanyu8 <wanghanyu8@jd.com>
2018-08-08 15:54:55 +0800

    xstream

---
+++ src/main/java/xstream/XStreamTest.java
@@ -0,0 +1,14 @@
+package xstream;
+
+import com.thoughtworks.xstream.XStream;
+
+/**
+ * @author Hanyu King
+ * @since 2018-08-08 15:51
+ */
+public class XStreamTest {
+    public static void main(String[] args) {
+        XStream xStream = new XStream();
+
+    }
+}
