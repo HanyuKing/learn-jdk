2eb20227d8899615d9e19fd830e18605bc7d1633
Hanyu.King <hanyuking2016@gmail.com>
2021-08-22 18:04:10 +0800

    P647

---
+++ src/main/java/leetcode/P647.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class P647 {
+    public static void main(String[] args) {
+        System.out.println(new P647().countSubstrings("abc"));
+        // c bc b ac abc ab a
+    }
+
+    private int count;
+    public int countSubstrings(String s) {
+        for(int i = 0; i < s.length(); i++) {
+            extendHuiWen(s, i, i);
+            extendHuiWen(s, i, i + 1);
+        }
+        return count;
+    }
+
+    private void extendHuiWen(String s, int start, int end) {
+        while (start >= 0 && end < s.length() && s.charAt(start) == s.charAt(end)) {
+            start--;
+            end++;
+
+            count++;
+        }
+        return;
+    }
+}
