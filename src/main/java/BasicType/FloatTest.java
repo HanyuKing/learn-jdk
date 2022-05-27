572e68ae306e8ad63a51e6ff5393e67d95e0de5e
Rain <hanyuking2016@gmail.com>
2022-05-28 02:18:02 +0800

    Float to Double 精度测试

---
+++ src/main/java/BasicType/FloatTest.java
@@ -0,0 +1,17 @@
+package BasicType;
+
+import org.junit.Test;
+
+/**
+ * @Author Hanyu.Wang
+ * @Date 2022/5/28 12:12
+ * @Description
+ * @Version 1.0
+ **/
+public class FloatTest {
+    @Test
+    public void test() {
+        float a = 1.125f;
+        System.out.println(a * 1d);
+    }
+}
