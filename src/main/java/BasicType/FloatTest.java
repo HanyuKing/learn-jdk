3c0e3e2b7c673f9762a42d9243f582c1756df5fc
Rain <hanyuking2016@gmail.com>
2022-05-28 02:25:54 +0800

    Float to Double 精度测试

--- src/main/java/BasicType/FloatTest.java
+++ src/main/java/BasicType/FloatTest.java
@@ -2,6 +2,8 @@ package BasicType;
 
 import org.junit.Test;
 
+import java.math.BigDecimal;
+
 /**
  * @Author Hanyu.Wang
  * @Date 2022/5/28 12:12
@@ -13,5 +15,21 @@ public class FloatTest {
     public void test() {
         float a = 1.125f;
         System.out.println(a * 1d);
+    }
+
+    @Test
+    public void test2() {
+        String a = "0.33";
+        double aa = Float.parseFloat(a) * 100d;
+        System.out.println(aa / 100);
+    }
+
+    @Test
+    public void test3() {
+        String a = "0.33";
+        System.out.println(new BigDecimal(a)
+                .multiply(new BigDecimal(100))
+                .divide(new BigDecimal(100))
+                .floatValue());
     }
 }
