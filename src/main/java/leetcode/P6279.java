0018c2efacdfac7d6480501215c7f208eef16c65
Rain <hanyuking2016@gmail.com>
2023-01-02 17:12:24 +0800

    commit

---
+++ src/main/java/leetcode/P6279.java
@@ -0,0 +1,59 @@
+package leetcode;
+
+import java.util.*;
+
+/**
+ * @Author Hanyu.Wang
+ * @Date 2023/1/1 22:43
+ * @Description
+ * @Version 1.0
+ **/
+public class P6279 {
+    public static void main(String[] args) {
+        System.out.println(new P6279().distinctPrimeFactors(new int[]{2,14,19,19,5,13,18,10,15,20}));
+    }
+
+    public int distinctPrimeFactors(int[] nums) {
+        List<Integer> allPrime = listAllPrime();
+        Set[] table = new HashSet[1001];
+        table[0] = new HashSet();
+        table[1] = new HashSet();
+
+        for (int i = 2; i <= 1000; i++) {
+            Set<Integer> sets = new HashSet<>();
+            int n = i;
+            for (int j = allPrime.size() - 1; j >= 0;) {
+                if (n % allPrime.get(j) == 0) {
+                    sets.add(allPrime.get(j));
+                    n = n / allPrime.get(j);
+                    continue;
+                }
+                j--;
+            }
+            table[i] = sets;
+        }
+
+        Set<Integer> cnt = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            cnt.addAll(table[nums[i]]);
+        }
+        return cnt.size();
+    }
+
+    private List<Integer> listAllPrime() {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 2; i <= 1000; i++) {
+            boolean is = true;
+            for (int j = 2; j <= (int) Math.sqrt(i) + 1; j++) {
+                if (i != j && i % j == 0) {
+                    is = false;
+                    break;
+                }
+            }
+            if (is) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+}
---
+++ src/main/java/leetcode/P6280.java
@@ -0,0 +1,49 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author Hanyu.Wang
+ * @Date 2023/1/2 01:28
+ * @Description
+ * @Version 1.0
+ **/
+public class P6280 {
+    public static void main(String[] args) {
+        int[] result = new P6280().closestPrimes(19, 31);
+        System.out.println("[" + result[0] + ", " + result[1] + "]");
+    }
+    static boolean[] notPrimes = new boolean[1000001];
+    static{
+        for(int i=2; i<1000001; i++){
+            if(notPrimes[i]){
+                continue;
+            }
+            for(int j=i+i; j<1000001; j+=i){
+                notPrimes[j] = true;
+            }
+        }
+    }
+    public int[] closestPrimes(int left, int right) {
+        int[] result = new int[]{-1, -1};
+        int pre = -1;
+        int minGap = Integer.MAX_VALUE;
+
+        for (int i = left; i <= right; i++) {
+            if (i == 1) {
+                continue;
+            }
+            if (!notPrimes[i]) {
+                if (pre != -1 && (i - pre < minGap)) {
+                    minGap = i - pre;
+                    result[0] = pre;
+                    result[1] = i;
+                }
+                pre = i;
+            }
+        }
+        return result;
+    }
+}
+
