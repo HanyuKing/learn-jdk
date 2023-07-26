2a93a2678bef8c13efa9e84f4c40f8548a3e1fdf
Rain <hanyuking2016@gmail.com>
2023-07-26 16:35:32 +0800

    car sql v2

--- src/main/java/uml/car/entryadd_stockout.sql
+++ src/main/java/uml/car/entryadd_stockout.sql
@@ -1,9 +1,9 @@
 /********************************** 入库单 **********************************/
 DROP
-  TABLE IF EXISTS `erp_stock_in_order`;
-CREATE TABLE `erp_stock_in_order` (
+  TABLE IF EXISTS `ctms_entry_order`;
+CREATE TABLE `ctms_entry_order` (
   `id` BIGINT (20) NOT NULL AUTO_INCREMENT,
-  `stock_in_order_id` VARCHAR (20) NOT NULL COMMENT '入库单号',
+  `entry_order_id` VARCHAR (20) NOT NULL COMMENT '入库单号',
   `related_id` VARCHAR (20) DEFAULT NULL COMMENT '关联单号',
   `wh_id` VARCHAR (10) NOT NULL COMMENT '目标仓库id',
   `type` TINYINT (3) NOT NULL COMMENT '入库类型id(补货需求-1, 采购入库-2, 匀仓入库-3)',
@@ -16,37 +16,55 @@ CREATE TABLE `erp_stock_in_order` (
   `is_fulfilled` TINYINT (2) DEFAULT NULL COMMENT '入库确认商品数量是否有差异(0-没有,1-有)',
   `is_cc` TINYINT (2) DEFAULT NULL COMMENT '入库确认商品中是否有残次(0-没有,1-有)',
   `remark` VARCHAR (200) DEFAULT NULL COMMENT '描述',
-  `creator_id` BIGINT (20) DEFAULT NULL COMMENT '创建人id',
-  `creator_name` VARCHAR (50) DEFAULT NULL COMMENT '创建人名字',
-  `is_delete` TINYINT (2) NOT NULL DEFAULT '1' COMMENT '是否逻辑删除 1-有效 2-已删除',
+  `operator` VARCHAR (50) DEFAULT NULL COMMENT '创建人名字',
+  `delete_flag` TINYINT ( 2 ) NOT NULL DEFAULT '0' COMMENT '是否逻辑删除 0-有效 1-已删除',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`),
-  KEY `idx_stock_in_order_id` (`stock_in_order_id`)
+  KEY `idx_entry_order_id` (`entry_order_id`)
 ) ENGINE = INNODB DEFAULT CHARSET = utf8 COMMENT = '入库单';
+DROP TABLE
+    IF
+        EXISTS `ctms_entry_order_item_flow`;
+CREATE TABLE `ctms_entry_order_item_flow` (
+  `id` BIGINT ( 20 ) NOT NULL AUTO_INCREMENT,
+  `entry_order_id` VARCHAR ( 20 ) NOT NULL COMMENT '入库单号',
+  `delivery_order_id` VARCHAR ( 20 ) NOT NULL COMMENT '运单号',
+  `ssu_id` BIGINT ( 20 ) NOT NULL DEFAULT '0' COMMENT '商品id',
+  `item_id` VARCHAR ( 128 ) NOT NULL COMMENT '商品编码',
+  `item_name` VARCHAR ( 128 ) DEFAULT NULL COMMENT '商品名称',
+  `receivable` INT ( 11 ) NOT NULL DEFAULT '0' COMMENT '应收数量',
+  `received_zp` INT ( 11 ) NOT NULL DEFAULT '0' COMMENT '实收正品数量',
+  `received_cc` INT ( 11 ) NOT NULL DEFAULT '0' COMMENT '实收残次数量',
+  `is_delete` TINYINT ( 2 ) NOT NULL DEFAULT '1' COMMENT '是否逻辑删除 1-有效 2-已删除',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY ( `id` ),
+  UNIQUE KEY `udx_entry_delivery_order_ssu_id` ( `entry_order_id`, `delivery_order_id` )
+) ENGINE = INNODB DEFAULT CHARSET = utf8 COMMENT = '入库单商品明细流水(幂等)';
 DROP
-  TABLE IF EXISTS `erp_stock_in_order_item`;
-CREATE TABLE `erp_stock_in_order_item` (
+  TABLE IF EXISTS `ctms_entry_order_item`;
+CREATE TABLE `ctms_entry_order_item` (
   `id` BIGINT (20) NOT NULL AUTO_INCREMENT,
-  `stock_in_order_id` VARCHAR (20) NOT NULL COMMENT '入库单号',
+  `entry_order_id` VARCHAR (20) NOT NULL COMMENT '入库单号',
   `ssu_id` BIGINT (20) NOT NULL DEFAULT '0' COMMENT '商品id',
-  `itemId` VARCHAR (128) NOT NULL COMMENT '商品编码',
-  `itemName` VARCHAR (128) DEFAULT NULL COMMENT '商品名称',
+  `item_id` VARCHAR (128) NOT NULL COMMENT '商品编码',
+  `item_name` VARCHAR (128) DEFAULT NULL COMMENT '商品名称',
   `receivable` INT (11) NOT NULL DEFAULT '0' COMMENT '应收数量',
   `received_zp` INT (11) NOT NULL DEFAULT '0' COMMENT '实收正品数量',
   `received_cc` INT (11) NOT NULL DEFAULT '0' COMMENT '实收残次数量',
-  `is_delete` TINYINT (2) NOT NULL DEFAULT '1' COMMENT '是否逻辑删除 1-有效 2-已删除',
+  `delete_flag` TINYINT ( 2 ) NOT NULL DEFAULT '0' COMMENT '是否逻辑删除 0-有效 1-已删除',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`),
-  KEY `idx_stock_in_order_id` (`stock_in_order_id`)
+  KEY `idx_entry_order_id` (`entry_order_id`)
 ) ENGINE = INNODB DEFAULT CHARSET = utf8 COMMENT = '入库单商品明细';
 /********************************** 入库单 end **********************************/
 
 /********************************** 出库单 **********************************/
 DROP
-  TABLE IF EXISTS `erp_stock_out_order`;
-CREATE TABLE `erp_stock_out_order` (
+  TABLE IF EXISTS `ctms_stock_out_order`;
+CREATE TABLE `ctms_stock_out_order` (
   `id` BIGINT (20) NOT NULL AUTO_INCREMENT COMMENT '主键',
   `stock_out_id` VARCHAR (20) NOT NULL COMMENT '出库单号',
   `related_id` VARCHAR (20) DEFAULT NULL COMMENT '关联单号',
@@ -74,27 +92,25 @@ CREATE TABLE `erp_stock_out_order` (
   `area` VARCHAR (50) DEFAULT NULL COMMENT '收件街道',
   `detail_address` VARCHAR (2048) NOT NULL DEFAULT '' COMMENT '详细收货地址',
   `create_from` VARCHAR (30) NOT NULL COMMENT '创建来源 ERP-供应链平台, M_CENTER-商家工作台',
-  `creator_id` BIGINT (20) DEFAULT NULL COMMENT '创建人id',
-  `creator_name` VARCHAR (50) DEFAULT NULL COMMENT '创建人名字',
+  `operator` VARCHAR (50) DEFAULT NULL COMMENT '创建人名字',
   `editor` VARCHAR (20) NOT NULL DEFAULT '' COMMENT '修改人',
-  `is_delete` TINYINT (2) NOT NULL DEFAULT '1' COMMENT '是否逻辑删除 1-有效 2-已删除',
+  `delete_flag` TINYINT ( 2 ) NOT NULL DEFAULT '0' COMMENT '是否逻辑删除 0-有效 1-已删除',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`),
   KEY `idx_stock_out_id` (`stock_out_id`)
 ) ENGINE = INNODB DEFAULT CHARSET = utf8 COMMENT '出库单';
 DROP
-  TABLE IF EXISTS `erp_stock_out_order_item`;
-CREATE TABLE `erp_stock_out_order_item` (
+  TABLE IF EXISTS `ctms_stock_out_order_item`;
+CREATE TABLE `ctms_stock_out_order_item` (
   `id` BIGINT (20) NOT NULL AUTO_INCREMENT COMMENT '主键',
   `stock_out_id` VARCHAR (20) NOT NULL COMMENT '出库单号',
   `ssu_id` BIGINT (20) NOT NULL COMMENT '商品id',
-  `product_code` VARCHAR (200) DEFAULT NULL COMMENT '商品编码',
-  `product_name` VARCHAR (200) DEFAULT NULL COMMENT '商品名称',
+  `item_id` VARCHAR (200) DEFAULT NULL COMMENT '商品编码',
+  `item_name` VARCHAR (200) DEFAULT NULL COMMENT '商品名称',
   `expect_number` INT (10) DEFAULT NULL COMMENT '预计出库数量',
-  `confirm_number` INT (10) DEFAULT NULL COMMENT '确认出库数量',
   `real_number` INT (10) DEFAULT NULL COMMENT '实际出库数量',
-  `is_delete` TINYINT (2) NOT NULL DEFAULT '1' COMMENT '是否逻辑删除 1-有效 2-已删除',
+  `delete_flag` TINYINT ( 2 ) NOT NULL DEFAULT '0' COMMENT '是否逻辑删除 0-有效 1-已删除',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`),
@@ -104,54 +120,70 @@ CREATE TABLE `erp_stock_out_order_item` (
 
 /********************************** 运单 ****************************************/
 DROP
-  TABLE IF EXISTS `erp_logistics_order`;
-CREATE TABLE `erp_logistics_order` (
+  TABLE IF EXISTS `ctms_delivery_order`;
+CREATE TABLE `ctms_delivery_order` (
   `id` BIGINT (20) NOT NULL AUTO_INCREMENT COMMENT '主键',
   `stock_out_id` VARCHAR (20) NOT NULL COMMENT '出库单号',
-  `logistics_order_id` VARCHAR (20) NOT NULL COMMENT '运单号',
+  `delivery_order_id` VARCHAR (20) NOT NULL COMMENT '运单号',
   `logistics_code` VARCHAR (50) NOT NULL DEFAULT '' COMMENT '快递公司代码',
   `logistics_name` VARCHAR (50) NOT NULL DEFAULT '' COMMENT '快递公司名称',
   `express_code` VARCHAR (50) NOT NULL DEFAULT '' COMMENT '快递单号',
   `status` VARCHAR (20) NOT NULL DEFAULT 'CREATE' COMMENT '运单状态(RECEIVED=已妥投, 在途=ONWAY)',
-  `is_delete` TINYINT (2) NOT NULL DEFAULT '1' COMMENT '是否逻辑删除 1-有效 2-已删除',
+  `delete_flag` TINYINT ( 2 ) NOT NULL DEFAULT '0' COMMENT '是否逻辑删除 0-有效 1-已删除',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`),
-  KEY `idx_logistics_order_id` (`logistics_order_id`)
+  KEY `idx_delivery_order_id` (`delivery_order_id`)
 ) ENGINE = INNODB DEFAULT CHARSET = utf8 COMMENT '运单';
 DROP
-  TABLE IF EXISTS `erp_logistics_order_item`;
-CREATE TABLE `erp_logistics_order_item` (
+  TABLE IF EXISTS `ctms_delivery_order_item`;
+CREATE TABLE `ctms_delivery_order_item` (
   `id` BIGINT (20) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `logistics_order_id` VARCHAR (20) NOT NULL COMMENT '运单号',
+  `delivery_order_id` VARCHAR (20) NOT NULL COMMENT '运单号',
   `ssu_id` BIGINT (20) NOT NULL COMMENT '商品id',
   `product_code` VARCHAR (200) DEFAULT NULL COMMENT '商品编码',
   `product_name` VARCHAR (200) DEFAULT NULL COMMENT '商品名称',
   `number` INT (10) DEFAULT NULL COMMENT '数量',
-  `is_delete` TINYINT (2) NOT NULL DEFAULT '1' COMMENT '是否逻辑删除 1-有效 2-已删除',
+  `delete_flag` TINYINT ( 2 ) NOT NULL DEFAULT '0' COMMENT '是否逻辑删除 0-有效 1-已删除',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`),
-  KEY `idx_logistics_order_id` (`logistics_order_id`)
+  KEY `idx_delivery_order_id` (`delivery_order_id`)
 ) ENGINE = INNODB DEFAULT CHARSET = utf8 COMMENT '运单明细';
 /********************************** 运单 end ****************************************/
 
+/********************************** 运单关系表  ****************************************/
+DROP
+    TABLE IF EXISTS `ctms_delivery_order_relation`;
+CREATE TABLE `ctms_delivery_order_relation`
+(
+    `id` BIGINT (20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+    `delivery_order_id` VARCHAR(20) NOT NULL COMMENT '运单号',
+    `relation_id`       TINYINT(2)  NOT NULL DEFAULT '1' COMMENT '是否逻辑删除 1-有效 2-已删除',
+    `type`              TINYINT(2)  NOT NULL COMMENT '是否逻辑删除 1-出库单 2-入库单',
+    `create_time`       datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    PRIMARY KEY (`id`),
+    KEY `idx_delivery_order_id` (`delivery_order_id`)
+) ENGINE = INNODB DEFAULT CHARSET = utf8 COMMENT '运单关系表';
+/********************************** 运单关系表 end ****************************************/
+
 /********************************** log ****************************************/
 DROP
-  TABLE IF EXISTS `erp_stock_in_order_log`;
-CREATE TABLE `erp_stock_in_order_log` (
+  TABLE IF EXISTS `ctms_entry_order_log`;
+CREATE TABLE `ctms_entry_order_log` (
   `id` BIGINT (20) NOT NULL AUTO_INCREMENT,
-  `stock_in_id` VARCHAR (20) NOT NULL COMMENT '入库单号',
+  `entry_id` VARCHAR (20) NOT NULL COMMENT '入库单号',
   `status` VARCHAR (20) NOT NULL COMMENT '入库单状态',
   `operator` BIGINT (20) DEFAULT NULL COMMENT '操作人id',
   `content` text COMMENT '入库单变化内容',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   PRIMARY KEY (`id`),
-  KEY `idx_stock_in_order_id` (`stock_in_id`)
+  KEY `idx_entry_order_id` (`entry_id`)
 ) ENGINE = INNODB DEFAULT CHARSET = utf8 COMMENT = '入库单操作日志';
 DROP
-  TABLE IF EXISTS `erp_stock_out_log`;
-CREATE TABLE `erp_stock_out_log` (
+  TABLE IF EXISTS `ctms_stock_out_log`;
+CREATE TABLE `ctms_stock_out_log` (
   `id` BIGINT (20) NOT NULL AUTO_INCREMENT COMMENT '主键',
   `stock_out_id` VARCHAR (20) DEFAULT NULL COMMENT '出库单号',
   `content` text COMMENT '出库单变化内容',
