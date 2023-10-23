f50e6417670ccf268f0be69111346e823c9f727e
Rain <hanyuking2016@gmail.com>
2023-10-23 11:27:05 +0800

    P2678

---
+++ src/main/java/leetcode/P2678.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @Author Hanyu.Wang
+ * @Date 2023/10/23 11:20
+ * @Description
+ * @Version 1.0
+ **/
+public class P2678 {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+        public int countSeniors(String[] details) {
+            return (int) Arrays.stream(details).filter(s -> Integer.parseInt(s.substring(11, 13)) > 60).count();
+        }
+    }
+}
