5ccdad8183afbe526a74d2e1b5d2dfb6a6ab0e7b
Rain <hanyuking2016@gmail.com>
2024-01-04 19:30:17 +0800

    some

--- src/main/java/BasicType/MapTest.java
+++ src/main/java/BasicType/MapTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @Author Hanyu.Wang
@@ -14,6 +15,41 @@ import java.util.Map;
  * @Version 1.0
  **/
 public class MapTest {
+
+    @Test
+    public void testNullValue() {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(1, null);
+        map.put(2, null);
+        System.out.println(map); // {1=null, 2=null}
+        System.out.println(map.get(1)); // null
+        System.out.println(map.get(111)); // null
+
+
+        map = new ConcurrentHashMap<>();
+        map.put(1, null); // NullPointerException
+        map.put(2, null);
+        System.out.println(map);
+        System.out.println(map.get(1));
+        System.out.println(map.get(111));
+    }
+
+    @Test
+    public void testNullKey() {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(null, 1);
+        map.put(null, 2);
+        System.out.println(map); // {null=2}
+        System.out.println(map.get(null)); // 2
+        System.out.println(map.get(123)); // null
+
+        map = new ConcurrentHashMap<>();
+        map.put(null, 1); // NullPointerException
+        map.put(null, 2);
+        System.out.println(map);
+        System.out.println(map.get(null));
+        System.out.println(map.get(123));
+    }
 
     @Test
     public void testTypeConvert() {
---
+++ src/main/java/leetcode/hot200/README.md
@@ -0,0 +1,3 @@
+script: var s = ""; a.list.forEach((ele) => {s += ele.leetcode.question_id + ',';}); console.log(s);
+
+1: 3,206,146,215,25,15,53,99990004,21,1,5,102,33,200,20,141,121,236,88,46,
