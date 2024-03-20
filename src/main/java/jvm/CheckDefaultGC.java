e21d600b4810993db5becd7fb9524c04575ab9f1
Rain <hanyuking2016@gmail.com>
2024-03-20 19:41:27 +0800

    CheckDefaultGC

--- src/main/java/jvm/CheckDefaultGC.java
+++ src/main/java/jvm/CheckDefaultGC.java
@@ -3,6 +3,9 @@ package jvm;
 import java.lang.management.GarbageCollectorMXBean;
 import java.lang.management.ManagementFactory;
 
+/**
+ * https://stackoverflow.com/questions/39929758/ps-marksweep-is-which-garbage-collector
+ */
 public class CheckDefaultGC {
     public static void main(String[] args) {
         System.out.println("Get JDK Default GC for jdk"
