d58e396657c9a17fd0d991c18c19348dc5c833ff
Rain <hanyuking2016@gmail.com>
2024-03-27 17:00:25 +0800

    ArrayList::remove

--- src/main/java/collection/ArrayListTest.java
+++ src/main/java/collection/ArrayListTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -30,6 +31,15 @@ public class ArrayListTest {
 
     }
 
+    /**
+     * https://www.geeksforgeeks.org/remove-element-arraylist-java/
+     *
+     * 1. Using remove() method by indexes(default)
+     * 2. Using remove() method by values
+     * 3. Using Iterator.remove() method
+     *
+     */
+
     @Test
     public void testRemove() {
         List<String> list = Lists.newArrayList("1", "2");
@@ -40,6 +50,39 @@ public class ArrayListTest {
         }
 
        // System.out.println(list);
+    }
+
+    @Test
+    public void testRemove2() {
+        List<String> list = Lists.newArrayList("1", "2");
+        int size = list.size();
+
+        for (int i = 0; i < size; i++) {
+            String val = list.get(i);
+            if ("2".equals(val)) {
+                list.remove(val);
+            }
+        }
+
+        System.out.println(list);
+    }
+
+    @Test
+    public void testTravelAndRemove() {
+        List<String> list = Lists.newArrayList("1", "2", "3");
+        Iterator<String> it = list.iterator();
+
+        while (it.hasNext()) {
+            String value = it.next();
+            if ("2".equals(value)) {
+                it.remove();
+            }
+        }
+
+        it = list.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
     }
 
     private static class AAA {
