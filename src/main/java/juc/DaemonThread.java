859a048e0c17139d88d72b430d7ca63baad85870
Rain <hanyuking2016@gmail.com>
2024-04-01 20:07:19 +0800

    DaemonThread

---
+++ src/main/java/juc/DaemonThread.java
@@ -0,0 +1,28 @@
+package juc;
+
+/**
+ * @Author Hanyu.Wang
+ * @Date 2024/4/1 20:05
+ * @Description
+ * @Version 1.0
+ **/
+public class DaemonThread {
+    public static void main(String[] args) {
+        Thread thread = new Thread(() -> {
+            while (true) {
+                System.out.println("I'm running...");
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        thread.setDaemon(true); // comment this line
+        thread.start();
+
+        System.out.println("main exit...");
+    }
+}
