91c2ab225715f5b4ba7d7919d35fa666522dccad
Rain <hanyuking2016@gmail.com>
2024-04-10 10:33:34 +0800

    threadlocal

--- src/main/java/threadlocal/ThreadPoolDemo.java
+++ src/main/java/threadlocal/ThreadPoolDemo.java
@@ -8,13 +8,17 @@ import java.util.concurrent.TimeUnit;
 public class ThreadPoolDemo {
     private static final ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor(5, 5, 1, TimeUnit.MINUTES, new LinkedBlockingQueue<>());
     public static void main(String[] args) throws InterruptedException {
+        Thread.sleep(30 * 1000);
         for (int i = 0; i < 100; ++i) {
+            System.out.println(i);
             poolExecutor.execute(new Runnable() {
                 @Override
                 public void run() {
-                    ThreadLocal<BigObject> threadLocal = new ThreadLocal<>();
+                    ThreadLocal<BigObject> threadLocal = new ThreadLocal<>(); // move up
                     threadLocal.set(new BigObject());
+
                     // 其他业务代码
+                    threadLocal.remove(); // comment
                 }
             });
             Thread.sleep(1000);
