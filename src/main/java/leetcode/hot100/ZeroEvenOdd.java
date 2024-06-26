3ca4248137c031100afe00e4d84889d016c2a0bc
Hanyu.King <hanyuking2016@gmail.com>
2024-06-26 21:48:20 +0800

    update

---
+++ src/main/java/leetcode/hot100/ZeroEvenOdd.java
@@ -0,0 +1,73 @@
+package leetcode.hot100;
+
+import java.util.concurrent.Semaphore;
+import java.util.function.IntConsumer;
+
+class ZeroEvenOdd {
+    private int n;
+    private Semaphore s1 = new Semaphore(1);
+    private Semaphore s2 = new Semaphore(0);
+    private Semaphore s3 = new Semaphore(0);
+
+    public ZeroEvenOdd(int n) {
+        this.n = n;
+    }
+
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public void zero(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 1; i <= n; i++) {
+            s1.acquire();
+            printNumber.accept(0);
+            if ((i & 1) == 0) {
+                s2.release();
+            } else {
+                s3.release();
+            }
+        }
+        System.out.println("zero");
+    }
+
+    public void even(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 2; i <= n; i += 2) {
+            s2.acquire();
+            printNumber.accept(i);
+            s1.release();
+        }
+        System.out.println("even");
+     }
+
+    public void odd(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 1; i <= n; i += 2) {
+            s3.acquire();
+            printNumber.accept(i);
+            s1.release();
+        }
+        System.out.println("odd");
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ZeroEvenOdd zeroEvenOdd = new ZeroEvenOdd(2);
+        IntConsumer intConsumer = System.out::print;
+        new Thread(() -> {
+            try {
+                zeroEvenOdd.zero(intConsumer);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+        new Thread(() -> {
+            try {
+                zeroEvenOdd.even(intConsumer);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+        new Thread(() -> {
+            try {
+                zeroEvenOdd.odd(intConsumer);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+    }
+}
