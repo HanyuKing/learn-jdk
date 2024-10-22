4bd5d7abb5c3396d3856dadcdd8b23272496ddc1
Rain <hanyuking2016@gmail.com>
2024-10-22 19:58:09 +0800

    code

--- src/main/java/algo/backtracking/Answer.java
+++ src/main/java/algo/backtracking/Answer.java
@@ -3,6 +3,10 @@ package algo.backtracking;
 import algo.hot200.Base;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @Author Hanyu.Wang
  * @Date 2024/10/21 15:03
@@ -13,7 +17,58 @@ public class Answer extends Base {
 
     @Test
     public void testP131() {
+        String s = "aab"; // [["a","a","b"],["aa","b"]]
+        // print(partition(s));
 
+        s = "abbab"; // abbab
+        print(partition(s));
+    }
+
+    boolean[][] flag;
+    List<List<String>> ret = new ArrayList<List<String>>();
+    List<String> ans = new ArrayList<String>();
+    int n;
+    public List<List<String>> partition(String s) {
+        n = s.length();
+        flag = new boolean[n][n];
+
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(flag[i], true);
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                flag[i][j] = flag[i + 1][j - 1] && s.charAt(i) == s.charAt(j);
+            }
+        }
+        doPartition2(s, 0);
+        return ret;
+    }
+
+    private void doPartition2(String s, int index) {
+        for (int i = index; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (flag[i][j]) {
+                    ans.add(s.substring(i, j + 1));
+                }
+            }
+            ret.add(ans);
+            ans = new ArrayList<>();
+        }
+    }
+
+    private void doPartition(String s, int index) {
+        if (index == n) {
+            ret.add(new ArrayList<>(ans));
+            return;
+        }
+        for (int j = index; j < n; j++) {
+            if (flag[index][j]) {
+                ans.add(s.substring(index, j + 1));
+                doPartition(s, j + 1);
+                ans.remove(ans.size() - 1);
+            }
+        }
     }
 
     @Test
