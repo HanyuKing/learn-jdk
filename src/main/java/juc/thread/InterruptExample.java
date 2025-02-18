1fb5305b1c56016e65b9f91583177f3ed81beba6
Rain <hanyuking2016@gmail.com>
2025-02-18 19:36:06 +0800

    testInterruptSleepThread

--- src/main/java/juc/thread/InterruptExample.java
+++ src/main/java/juc/thread/InterruptExample.java
@@ -2,6 +2,8 @@ package juc.thread;
 
 import org.junit.Test;
 
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.LockSupport;
 
@@ -57,6 +59,32 @@ public class InterruptExample {
 
         }
     }
+
+    private static class MyThread3 extends Thread {
+
+        public void run() {
+            System.out.println("Thread3 run");
+            long start = System.currentTimeMillis();
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                System.out.println("Thread3 sleep " + (System.currentTimeMillis() - start) + " ms");
+                throw new RuntimeException(e);
+            }
+
+        }
+    }
+
+    @Test
+    public void testInterruptSleepThread() throws InterruptedException {
+        MyThread3 t = new MyThread3();
+        t.start();
+        long sleep = ThreadLocalRandom.current().nextInt(1000);
+        System.out.println("sleep: " + sleep + " ms");
+        Thread.sleep(sleep);
+        t.interrupt();
+    }
+
 
     public static void main(String[] args) throws InterruptedException {
         Thread thread1 = new MyThread1();
